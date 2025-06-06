c6d9d980c9251c9a2584005da7a8f158bda6bbd3
Shashank <mshashank02@gmail.com>
2025-06-05 22:49:21 -0700

    changed test

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-# Run this in Python
 import gymnasium as gym
-env = gym.make("FetchReach-v3")
+import gymnasium_robotics
+
+gym.register_envs(gymnasium_robotics)
+
+env = gym.make('FetchReach-v3', max_episode_steps=100)
 obs, _ = env.reset()
 print("Observation keys:", obs.keys())
 
